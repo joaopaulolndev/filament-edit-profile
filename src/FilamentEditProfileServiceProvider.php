a2d4670ee6313b4fd37452a78c5991bff8a91f15
joaopaulolndev <joaopaulolndev@users.noreply.github.com>
2024-05-17 10:06:39 +0000

    Fix styling

--- src/FilamentEditProfileServiceProvider.php
+++ src/FilamentEditProfileServiceProvider.php
@@ -65,10 +65,10 @@ class FilamentEditProfileServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         // Asset Registration
-//        FilamentAsset::register(
-//            $this->getAssets(),
-//            $this->getAssetPackageName()
-//        );
+        //        FilamentAsset::register(
+        //            $this->getAssets(),
+        //            $this->getAssetPackageName()
+        //        );
 
         FilamentAsset::registerScriptData(
             $this->getScriptData(),
