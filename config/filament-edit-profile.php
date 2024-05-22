f4428f56eb8594c623fa40e29d8d266725738592
Joao Nascimento <joao.nascimento.ext@tlscontact.com>
2024-05-22 21:07:36 +0100

    Update config file

--- config/filament-edit-profile.php
+++ config/filament-edit-profile.php
@@ -1,43 +1,43 @@
 <?php
 
 return [
-    'show_custom_fields' => true,
-    'custom_fields' => [
-        'custom_field_1' => [
-            'type' => 'text',
-            'label' => 'Custom Textfield 1',
-            'placeholder' => 'Custom Field 1',
-            'required' => true,
-            'rules' => 'required|string|max:255',
-        ],
-        'custom_field_2' => [
-            'type' => 'select',
-            'label' => 'Custom Select 2',
-            'placeholder' => 'Select',
-            'required' => true,
-            'options' => [
-                'option_1' => 'Option 1',
-                'option_2' => 'Option 2',
-                'option_3' => 'Option 3',
-            ],
-        ],
-        'custom_field_3' => [
-            'type' => 'textarea',
-            'label' => 'Custom Textarea 3',
-            'placeholder' => 'Textarea',
-            'rows' => '3',
-            'required' => true,
-        ],
-        'custom_field_4' => [
-            'type' => 'datetime',
-            'label' => 'Custom Datetime 4',
-            'placeholder' => 'Datetime',
-            'seconds' => false,
-        ],
-        'custom_field_5' => [
-            'type' => 'boolean',
-            'label' => 'Custom Boolean 5',
-            'placeholder' => 'Boolean',
-        ],
-    ],
+//    'show_custom_fields' => true,
+//    'custom_fields' => [
+//        'custom_field_1' => [
+//            'type' => 'text',
+//            'label' => 'Custom Textfield 1',
+//            'placeholder' => 'Custom Field 1',
+//            'required' => true,
+//            'rules' => 'required|string|max:255',
+//        ],
+//        'custom_field_2' => [
+//            'type' => 'select',
+//            'label' => 'Custom Select 2',
+//            'placeholder' => 'Select',
+//            'required' => true,
+//            'options' => [
+//                'option_1' => 'Option 1',
+//                'option_2' => 'Option 2',
+//                'option_3' => 'Option 3',
+//            ],
+//        ],
+//        'custom_field_3' => [
+//            'type' => 'textarea',
+//            'label' => 'Custom Textarea 3',
+//            'placeholder' => 'Textarea',
+//            'rows' => '3',
+//            'required' => true,
+//        ],
+//        'custom_field_4' => [
+//            'type' => 'datetime',
+//            'label' => 'Custom Datetime 4',
+//            'placeholder' => 'Datetime',
+//            'seconds' => false,
+//        ],
+//        'custom_field_5' => [
+//            'type' => 'boolean',
+//            'label' => 'Custom Boolean 5',
+//            'placeholder' => 'Boolean',
+//        ],
+//    ],
 ];
