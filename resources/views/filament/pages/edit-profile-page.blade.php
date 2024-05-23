622ed617a725046930b207ec6e253d004c0fdab8
Rômulo Ramos <rmsramos@gmail.com>
2024-05-23 12:08:43 -0300

    configured sanctum component

--- resources/views/filament/pages/edit-profile-page.blade.php
+++ resources/views/filament/pages/edit-profile-page.blade.php
@@ -28,6 +28,18 @@
         </x-filament-panels::form>
     @endif
 
+    @if($this->shouldShowSanctumTokens())
+        <x-filament::section aside>
+            <x-slot name="heading">
+                {{ __('filament-edit-profile::default.token_section_title') }}
+            </x-slot>
+            <x-slot name="description">
+                {{ __('filament-edit-profile::default.token_section_description') }}
+            </x-slot>
+
+            @livewire('sanctum-tokens')
+        </x-filament::section>
+    @endif
 
     @if($this->shouldShowDeleteAccountForm())
         <x-filament-panels::form>
--- src/FilamentEditProfilePlugin.php
+++ src/FilamentEditProfilePlugin.php
@@ -6,27 +6,33 @@ use Closure;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
 use Filament\Support\Concerns\EvaluatesClosures;
+use Joaopaulolndev\FilamentEditProfile\Livewire\SanctumTokens;
 use Joaopaulolndev\FilamentEditProfile\Pages\EditProfilePage;
+use Livewire\Livewire;
 
 class FilamentEditProfilePlugin implements Plugin
 {
     use EvaluatesClosures;
 
-    public Closure | bool $access = true;
+    public Closure|bool $access = true;
 
-    public Closure | bool $shouldRegisterNavigation = true;
+    public Closure|bool $shouldRegisterNavigation = true;
 
-    public Closure | int $sort = 90;
+    public Closure|int $sort = 90;
 
-    public Closure | string $icon = '';
+    public Closure|string $icon = '';
 
-    public Closure | string $navigationGroup = '';
+    public Closure|string $navigationGroup = '';
 
-    public Closure | string $title = '';
+    public Closure|string $title = '';
 
-    public Closure | string $navigationLabel = '';
+    public Closure|string $navigationLabel = '';
 
-    public Closure | bool $shouldShowDeleteAccountForm = true;
+    public Closure|bool $shouldShowDeleteAccountForm = true;
+
+    protected $sanctumTokens = false;
+
+    protected $sanctumPermissions = ['create', 'view', 'update', 'delete'];
 
     public function getId(): string
     {
@@ -49,7 +55,9 @@ class FilamentEditProfilePlugin implements Plugin
 
     public function boot(Panel $panel): void
     {
-        //
+        if ($this->sanctumTokens) {
+            Livewire::component('sanctum-tokens', SanctumTokens::class);
+        }
     }
 
     public static function make(): static
@@ -65,7 +73,7 @@ class FilamentEditProfilePlugin implements Plugin
         return $plugin;
     }
 
-    public function setTitle(Closure | string $value = ''): static
+    public function setTitle(Closure|string $value = ''): static
     {
         $this->title = $value;
 
@@ -77,7 +85,7 @@ class FilamentEditProfilePlugin implements Plugin
         return ! empty($this->title) ? $this->evaluate($this->title) : null;
     }
 
-    public function setNavigationLabel(Closure | string $value = ''): static
+    public function setNavigationLabel(Closure|string $value = ''): static
     {
         $this->navigationLabel = $value;
 
@@ -89,7 +97,7 @@ class FilamentEditProfilePlugin implements Plugin
         return ! empty($this->navigationLabel) ? $this->evaluate($this->navigationLabel) : null;
     }
 
-    public function setNavigationGroup(Closure | string $value = ''): static
+    public function setNavigationGroup(Closure|string $value = ''): static
     {
         $this->navigationGroup = $value;
 
@@ -101,7 +109,7 @@ class FilamentEditProfilePlugin implements Plugin
         return ! empty($this->navigationGroup) ? $this->evaluate($this->navigationGroup) : null;
     }
 
-    public function setIcon(Closure | string $value = ''): static
+    public function setIcon(Closure|string $value = ''): static
     {
         $this->icon = $value;
 
@@ -113,7 +121,7 @@ class FilamentEditProfilePlugin implements Plugin
         return ! empty($this->icon) ? $this->evaluate($this->icon) : null;
     }
 
-    public function setSort(Closure | int $value = 100): static
+    public function setSort(Closure|int $value = 100): static
     {
         $this->sort = $value;
 
@@ -125,7 +133,7 @@ class FilamentEditProfilePlugin implements Plugin
         return $this->evaluate($this->sort);
     }
 
-    public function canAccess(Closure | bool $value = true): static
+    public function canAccess(Closure|bool $value = true): static
     {
         $this->access = $value;
 
@@ -137,7 +145,7 @@ class FilamentEditProfilePlugin implements Plugin
         return $this->evaluate($this->access);
     }
 
-    public function shouldRegisterNavigation(Closure | bool $value = true): static
+    public function shouldRegisterNavigation(Closure|bool $value = true): static
     {
         $this->shouldRegisterNavigation = $value;
 
@@ -149,7 +157,7 @@ class FilamentEditProfilePlugin implements Plugin
         return $this->evaluate($this->shouldRegisterNavigation);
     }
 
-    public function shouldShowDeleteAccountForm(Closure | bool $value = true): static
+    public function shouldShowDeleteAccountForm(Closure|bool $value = true): static
     {
         $this->shouldShowDeleteAccountForm = $value;
 
@@ -159,5 +167,30 @@ class FilamentEditProfilePlugin implements Plugin
     public function getShouldShowDeleteAccountForm(): bool
     {
         return $this->evaluate($this->shouldShowDeleteAccountForm);
+    }
+
+    public function getshouldShowSanctumTokens(): bool
+    {
+        return $this->evaluate($this->sanctumTokens);
+    }
+
+    public function shouldShowSanctumTokens(bool $condition = true, ?array $permissions = null)
+    {
+        $this->sanctumTokens = $condition;
+
+        if (! is_null($permissions)) {
+            $this->sanctumPermissions = $permissions;
+        }
+
+        return $this;
+    }
+
+    public function getSanctumPermissions(): array
+    {
+        return collect($this->sanctumPermissions)->mapWithKeys(function ($item, $key) {
+            $key = is_string($key) ? $key : strtolower($item);
+
+            return [$key => $item];
+        })->toArray();
     }
 }
