5616d3e30a90f60338a4a0c559bccae7783b1611
Rômulo Ramos <rmsramos@gmail.com>
2024-06-13 15:46:54 -0300

    edit profile component created

---
+++ resources/views/livewire/edit-profile-form.blade.php
@@ -0,0 +1,11 @@
+<x-filament-panels::form wire:submit="updateProfile">
+    {{ $this->form }}
+
+    <div class="fi-form-actions">
+        <div class="flex flex-row-reverse flex-wrap items-center gap-3 fi-ac">
+            <x-filament::button type="submit">
+                {{ __('filament-edit-profile::default.save') }}
+            </x-filament::button>
+        </div>
+    </div>
+</x-filament-panels::form>
