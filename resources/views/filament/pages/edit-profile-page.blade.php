4dad57f24c41832d06ff8a725fc7a718c5280ee7
Rômulo Ramos <rmsramos@gmail.com>
2024-06-13 15:51:48 -0300

    edit profile page refactoring

--- resources/views/filament/pages/edit-profile-page.blade.php
+++ resources/views/filament/pages/edit-profile-page.blade.php
@@ -1,55 +1,7 @@
 <x-filament-panels::page>
-    <x-filament-panels::form wire:submit="updateProfile">
-        {{ $this->editProfileForm }}
-
-        <x-filament-panels::form.actions
-            alignment="right"
-            :actions="$this->getUpdateProfileFormActions()"
-        />
-    </x-filament-panels::form>
-
-    <x-filament-panels::form wire:submit="updatePassword">
-        {{ $this->editPasswordForm }}
-
-        <x-filament-panels::form.actions
-            alignment="right"
-            :actions="$this->getUpdatePasswordFormActions()"
-        />
-    </x-filament-panels::form>
-
-    @if(config('filament-edit-profile.show_custom_fields'))
-        <x-filament-panels::form wire:submit="updateCustomFields">
-            {{ $this->customFieldsForm }}
-
-            <x-filament-panels::form.actions
-                alignment="right"
-                :actions="$this->getUpdateCustomFieldsFormActions()"
-            />
-        </x-filament-panels::form>
-    @endif
-
-    @if($this->shouldShowSanctumTokens())
-        <x-filament::section aside>
-            <x-slot name="heading">
-                {{ __('filament-edit-profile::default.token_section_title') }}
-            </x-slot>
-            <x-slot name="description">
-                {{ __('filament-edit-profile::default.token_section_description') }}
-            </x-slot>
-
-            @livewire('sanctum-tokens')
-        </x-filament::section>
-    @endif
-
-    @if($this->shouldShowBrowserSessionsForm())
-        <x-filament-panels::form>
-            {{ $this->browserSessionsForm }}
-        </x-filament-panels::form>
-    @endif
-
-    @if($this->shouldShowDeleteAccountForm())
-        <x-filament-panels::form>
-            {{ $this->deleteAccountForm }}
-        </x-filament-panels::form>
-    @endif
+    @foreach ($this->getRegisteredCustomProfileComponents() as $component)
+        @unless(is_null($component))
+            @livewire($component)
+        @endunless
+    @endforeach
 </x-filament-panels::page>
