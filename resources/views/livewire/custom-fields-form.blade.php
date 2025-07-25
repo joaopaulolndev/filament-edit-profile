555cf1ba8b0478ca919f37b241bf353d23fc63c2
agcodex <agcodex01@gmail.com>
2025-07-26 01:00:06 +0800

    apply review comment

--- resources/views/livewire/custom-fields-form.blade.php
+++ resources/views/livewire/custom-fields-form.blade.php
@@ -1,7 +1,7 @@
 <form wire:submit="updateCustomFields">
     {{ $this->form }}
 
-    <div class="fi-form- mt-4">
+    <div class="fi-form-actions mt-4">
         <div class="flex flex-row-reverse flex-wrap items-center gap-3 fi-ac">
             <x-filament::button type="submit">
                 {{ __('filament-edit-profile::default.save') }}
