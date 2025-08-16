25b0562c2f73c5a0ea11b6972d09583a5af7f3e3
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-08-16 16:05:17 -0300

    feat(middleware): enhance theme color support with palette generation
    Update `SetUserThemeColor` middleware to generate a color palette using `Color::generatePalette`. This improves theme customization by providing a richer color scheme based on the user-defined `theme_color`.

--- src/Http/Middleware/SetUserThemeColor.php
+++ src/Http/Middleware/SetUserThemeColor.php
@@ -3,6 +3,7 @@
 namespace Joaopaulolndev\FilamentEditProfile\Http\Middleware;
 
 use Closure;
+use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +18,7 @@ class SetUserThemeColor
 
         if ($user && filled($user->getAttributeValue($theme_color))) {
             FilamentColor::register([
-                'primary' => $user->getAttributeValue($theme_color),
+                'primary' => Color::generatePalette($user->getAttributeValue($theme_color)),
             ]);
         }
 
